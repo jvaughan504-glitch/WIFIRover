dd6bdb83c709d3775bc351aa55a7b7f8418dfc7c
Jayson Vaughan <jvaughan504@gmail.com>
2025-09-22 22:44:36 -0400

    Update VehicleManager.ino

--- VehicleManager.ino
+++ VehicleManager.ino
@@ -116,7 +116,7 @@ void applyControls() {
   digitalWrite(HORN_PIN, hornOn ? HIGH : LOW);
   digitalWrite(LIGHTS_PIN, lightsOn ? HIGH : LOW);
 }
-=======
+
  Servo rearServo;
  
  // === ESC ===
@@ -144,26 +144,26 @@ void applyControls() {
  int bufPos = 0;
  
 +void activateFailsafe();
-+
+
  void setup() {
    SERIAL_ESP32.begin(BAUD);
-+  lastCommandTime = millis();
-+
+ lastCommandTime = millis();
+
    frontServo.attach(FRONT_SERVO_PIN);
    rearServo.attach(REAR_SERVO_PIN);
    esc.attach(ESC_PIN);
--  
-+
+
+
    pinMode(HORN_PIN, OUTPUT);
    pinMode(LIGHTS_PIN, OUTPUT);
  
--  // Initialize outputs
--  frontServo.write(90);
--  rearServo.write(90);
--  esc.write(90);
--  digitalWrite(HORN_PIN, LOW);
--  digitalWrite(LIGHTS_PIN, LOW);
-+  activateFailsafe();
+  // Initialize outputs
+  frontServo.write(90);
+  rearServo.write(90);
+  esc.write(90);
+  digitalWrite(HORN_PIN, LOW);
+  digitalWrite(LIGHTS_PIN, LOW);
+  activateFailsafe();
  }
  
  void loop() {
@@ -180,19 +180,19 @@ void applyControls() {
      char c = SERIAL_ESP32.read();
  
      if (c == '\n') {
--      serialBuffer[bufPos] = '\0'; 
-+      serialBuffer[bufPos] = '\0';
+      serialBuffer[bufPos] = '\0'; 
+      serialBuffer[bufPos] = '\0';
        parseCommands(serialBuffer);
--      bufPos = 0; 
-+      bufPos = 0;
-+      lastCommandTime = millis();
+      bufPos = 0; 
+      bufPos = 0;
+      lastCommandTime = millis();
      } else if (bufPos < BUF_SIZE - 1) {
        serialBuffer[bufPos++] = c;
-+    } else {
-+      bufPos = 0; // overflow protection
+    } else {
+      bufPos = 0; // overflow protection
      }
    }
--  lastCommandTime = millis();
+  lastCommandTime = millis();
  }
  
  // ----------------------
@@ -201,44 +201,44 @@ void applyControls() {
  void parseCommands(char* packet){
    char* token = strtok(packet,";");
    while(token != NULL){
--    if(strncmp(token,"STEER:",6)==0) steering = atoi(token+6);
--    else if(strncmp(token,"THROT:",6)==0) throttle = atoi(token+6);
--    else if(strncmp(token,"HORN:",5)==0) hornOn = atoi(token+5);
--    else if(strncmp(token,"LIGHTS:",7)==0) lightsOn = atoi(token+7);
--    else if(strncmp(token,"AUTO:",5)==0) autonomousMode = atoi(token+5);
-+    if(strncmp(token,"STEER:",6)==0) {
-+      steering = constrain(atoi(token+6), 0, 180);
-+    }
-+    else if(strncmp(token,"THROT:",6)==0) {
-+      throttle = constrain(atoi(token+6), 0, 180);
-+    }
-+    else if(strncmp(token,"HORN:",5)==0) {
-+      hornOn = atoi(token+5) != 0;
-+    }
-+    else if(strncmp(token,"LIGHTS:",7)==0) {
-+      lightsOn = atoi(token+7) != 0;
-+    }
-+    else if(strncmp(token,"AUTO:",5)==0) {
-+      autonomousMode = atoi(token+5) != 0;
-+    }
-+    else if(strcmp(token, "CMD FAILSAFE") == 0 || strcmp(token, "FAILSAFE") == 0) {
-+      activateFailsafe();
-+    }
-     token = strtok(NULL,";");
-   }
- }
+    if(strncmp(token,"STEER:",6)==0) steering = atoi(token+6);
+    else if(strncmp(token,"THROT:",6)==0) throttle = atoi(token+6);
+    else if(strncmp(token,"HORN:",5)==0) hornOn = atoi(token+5);
+    else if(strncmp(token,"LIGHTS:",7)==0) lightsOn = atoi(token+7);
+    else if(strncmp(token,"AUTO:",5)==0) autonomousMode = atoi(token+5);
+    if(strncmp(token,"STEER:",6)==0) {
+      steering = constrain(atoi(token+6), 0, 180);
+    }
+    else if(strncmp(token,"THROT:",6)==0) {
+      throttle = constrain(atoi(token+6), 0, 180);
+    }
+    else if(strncmp(token,"HORN:",5)==0) {
+      hornOn = atoi(token+5) != 0;
+    }
+    else if(strncmp(token,"LIGHTS:",7)==0) {
+      lightsOn = atoi(token+7) != 0;
+    }
+    else if(strncmp(token,"AUTO:",5)==0) {
+     autonomousMode = atoi(token+5) != 0;
+    }
+    else if(strcmp(token, "CMD FAILSAFE") == 0 || strcmp(token, "FAILSAFE") == 0) {
+      activateFailsafe();
+    }
+    token = strtok(NULL,";");
+  }
+}
  
  // ----------------------
  // Check failsafe
  // ----------------------
  void checkFailsafe() {
    if(millis() - lastCommandTime > TIMEOUT){
--    steering = 90;
--    throttle = 90;
--    hornOn = false;
--    lightsOn = false;
--    autonomousMode = false;
-+    activateFailsafe();
+   steering = 90;
+    throttle = 90;
+    hornOn = false;
+    lightsOn = false;
+    autonomousMode = false;
+    activateFailsafe();
    }
  }
  
@@ -252,18 +252,17 @@ void applyControls() {
    digitalWrite(HORN_PIN, hornOn ? HIGH : LOW);
    digitalWrite(LIGHTS_PIN, lightsOn ? HIGH : LOW);
  }
-+
-+void activateFailsafe() {
-+  steering = 90;
-+  throttle = 90;
-+  hornOn = false;
-+  lightsOn = false;
-+  autonomousMode = false;
-+
-+  frontServo.write(steering);
-+  rearServo.write(steering);
-+  esc.write(throttle);
-+  digitalWrite(HORN_PIN, LOW);
-+  digitalWrite(LIGHTS_PIN, LOW);
-+}
 
+void activateFailsafe() {
+  steering = 90;
+  throttle = 90;
+  hornOn = false;
+  lightsOn = false;
+  autonomousMode = false;
+
+  frontServo.write(steering);
+  rearServo.write(steering);
+  esc.write(throttle);
+  digitalWrite(HORN_PIN, LOW);
+  digitalWrite(LIGHTS_PIN, LOW);
+}
